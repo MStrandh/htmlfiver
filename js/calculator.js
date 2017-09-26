5c5f2fc9290f0ae1411573b782bf2f294a7fa0aa
Magnus Strandh <magnus@pixelmath.se>
2017-09-26 21:45:20 +0200

    Calculator basic setup

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,3 @@
-console.log("CALCULATOR");
+function Calculator() {
+	
+};
--- js_test/calculator_test.js
+++ js_test/calculator_test.js
@@ -1,6 +1,6 @@
 describe("A calculator", function() {
 
 	it("Can sum", function () {
-
+		var calc = new Calculator();
 	})
 });
