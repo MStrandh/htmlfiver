89a5f8fd27d13475d6f9901780511acd58b06c14
Magnus Strandh <magnus.strandh@king.com>
2017-10-19 15:08:33 +0200

    P5 sketch test

--- dev/js/sketch.js
+++ dev/js/sketch.js
@@ -1,7 +1,17 @@
-function setup() {
+var posX = 400;
+var posY = 300;
 
+var radius = 20;
+
+function setup() {
+	createCanvas(800, 600);	
 }
 
 function draw() {
-	
+	clear();
+	background("#DDDDDD");
+
+	ellipse(posX, posY, radius, radius);
+
+	posX -= 2;
 }
