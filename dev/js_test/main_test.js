82e956904c855a4957b4948cd94e5b497e9a0b1c
Magnus Strandh <magnus.strandh@king.com>
2017-11-01 11:56:03 +0100

    Added comments

--- dev/js_test/main_test.js
+++ dev/js_test/main_test.js
@@ -2,12 +2,16 @@ describe("Boat tools", function() {
 
 	describe("has a function called getHarbour", function() {
 
+
+		/***************** TEST ****************/
 		// it("that exists", function() {
 		// 	getHarbour();
 
 		// 	expect(getHarbour).toBeDefined();
 		// });
 
+
+		/***************** TEST ****************/
 		// it("that returns a harbour when called", function() {
 		// 	var portName = getHarbour();
 
@@ -17,30 +21,39 @@ describe("Boat tools", function() {
 
 	describe("has a function called getBoatName", function() {
 
+		/***************** TEST ****************/
 		// it("that exists", function() {
 		// 	getBoatName();
 
 		// 	expect(getBoatName).toBeDefined();
 		// });
 
+
+		/***************** TEST ****************/
 		// it("that accepts a parameter", function() {
 		// 	var boatName = getBoatName("Jehander");
 
 		// 	expect(getBoatName.length).toBe(1, "getBoatName needs to accept one parameter");
 		// });
 
+
+		/***************** TEST ****************/
 		// it("that returns a boat name string when called with a boat name", function() {
 		// 	var boatName = getBoatName("Jehander");
 
 		// 	expect(boatName).toBe("Båten Jehander", "It should return \"Båten Jehander\"");
 		// });
 
+
+		/***************** TEST ****************/
 		// it("that handles different boat names", function() {
 		// 	var boatName = getBoatName("Östanvik");
 
 		// 	expect(boatName).toBe("Båten Östanvik", "It should return \"Båten Östanvik\"");
 		// });
 
+
+		/***************** TEST ****************/
 		// it("that returns an empty string when no parameter is passed in", function() {
 		// 	var boatName = getBoatName("");
 
