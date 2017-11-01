c62f6db6854153aebacf95a577e2c4cd3f79e1bd
Magnus Strandh <magnus.strandh@king.com>
2017-11-01 11:36:50 +0100

    Boat tests

---
+++ dev/js_test/main_test.js
@@ -0,0 +1,50 @@
+describe("Boat tools", function() {
+
+	describe("has a function called getHarbour", function() {
+
+		// it("that exists", function() {
+		// 	getHarbour();
+
+		// 	expect(getHarbour).toBeDefined();
+		// });
+
+		// it("that returns a harbour when called", function() {
+		// 	var portName = getHarbour();
+
+		// 	expect(portName).toEqual("Gröndal");
+		// });
+	});
+
+	describe("has a function called getBoatName", function() {
+
+		// it("that exists", function() {
+		// 	getBoatName();
+
+		// 	expect(getBoatName).toBeDefined();
+		// });
+
+		// it("that accepts a parameter", function() {
+		// 	var boatName = getBoatName("Jehander");
+
+		// 	expect(getBoatName.length).toBe(1, "getBoatName needs to accept one parameter");
+		// });
+
+		// it("that returns a boat name string when called with a boat name", function() {
+		// 	var boatName = getBoatName("Jehander");
+
+		// 	expect(boatName).toBe("Båten Jehander", "It should return \"Båten Jehander\"");
+		// });
+
+		// it("that handles different boat names", function() {
+		// 	var boatName = getBoatName("Östanvik");
+
+		// 	expect(boatName).toBe("Båten Östanvik", "It should return \"Båten Östanvik\"");
+		// });
+
+		// it("that returns an empty string when no parameter is passed in", function() {
+		// 	var boatName = getBoatName("");
+
+		// 	expect(boatName).toBe("", "Hint: use if-clause to check the parameter");
+		// });
+	});
+});
